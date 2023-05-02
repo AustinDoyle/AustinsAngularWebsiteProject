028a01ba6aa25aae5619b476af15f14e72699fd6
Austin Doyle <austindoyle72@gmail.com>
2023-05-01 20:22:20 -0400

    update two column test

--- src/app/app-components/two-column-image-text/two-column-image-text.component.spec.ts
+++ src/app/app-components/two-column-image-text/two-column-image-text.component.spec.ts
@@ -1,6 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TwoColumnImageTextComponent } from './two-column-image-text.component';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-host',
+  template: `
+    <app-two-column-image-text>
+      <p>Hello, world!</p>
+    </app-two-column-image-text>
+  `,
+})
+class TestHostComponent {}
 
 describe('TwoColumnImageTextComponent', () => {
   let component: TwoColumnImageTextComponent;
@@ -8,7 +19,7 @@ describe('TwoColumnImageTextComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [TwoColumnImageTextComponent],
+      declarations: [TwoColumnImageTextComponent, TestHostComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TwoColumnImageTextComponent);
@@ -18,5 +29,25 @@ describe('TwoColumnImageTextComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should bind imageUrl to img src attribute', () => {
+    const imageUrl = 'https://example.com/image.jpg';
+    component.imageUrl = imageUrl;
+    fixture.detectChanges();
+  
+    const compiled = fixture.nativeElement;
+    const img = compiled.querySelector('img');
+  
+    expect(img.getAttribute('src')).toBe(imageUrl);
+  });
+
+  it('should render projected content', () => {
+    const hostFixture = TestBed.createComponent(TestHostComponent);
+    hostFixture.detectChanges();
+  
+    const compiled = hostFixture.nativeElement;
+    const cols = compiled.querySelectorAll('.col-md-6');
+    expect(cols[1].innerHTML).toContain('<p>Hello, world!</p>');
   });
 });
