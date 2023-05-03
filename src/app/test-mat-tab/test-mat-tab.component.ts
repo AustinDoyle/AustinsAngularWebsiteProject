972e3d8ff48890fa2a28dc0dbe79868b95b41259
Austin Doyle <austindoyle72@gmail.com>
2023-05-03 12:20:52 -0400

    add angular material and test that it works

---
+++ src/app/test-mat-tab/test-mat-tab.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-mat-tab',
+  templateUrl: './test-mat-tab.component.html',
+  styleUrls: ['./test-mat-tab.component.css']
+})
+export class TestMatTabComponent {
+
+}
